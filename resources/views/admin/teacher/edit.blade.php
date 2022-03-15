da314d0c20415b305690df801eb17b146430ddfb
khorshed31 <khorshedaom3517@gamil.com>
2022-03-15 13:12:29 +0600

    SMS V5

--- resources/views/admin/teacher/edit.blade.php
+++ resources/views/admin/teacher/edit.blade.php
@@ -5,44 +5,46 @@
         <div class="col-lg-12">
             <div class="card">
                 <div class="card-body">
-                    <h4 class="card-title mb-4">Add Teacher Form</h4>
+                    <h4 class="card-title mb-4">Update Teacher Form</h4>
                     <p class="text-center text-success">{{ Session::get('message') }}</p>
-                    <form action="{{ route('new-teacher') }}" method="POST" enctype="multipart/form-data">
+                    <form action="{{ route('update-teacher', ['id' => $teacher->id]) }}" method="POST" enctype="multipart/form-data">
                         @csrf
                         <div class="form-group row mb-4">
                             <label class="col-sm-3 col-form-label">Full name</label>
                             <div class="col-sm-9">
-                                <input type="text" name="name" class="form-control"/>
+                                <input type="text" value="{{ $teacher->name }}" name="name" class="form-control"/>
                             </div>
                         </div>
                         <div class="form-group row mb-4">
                             <label class="col-sm-3 col-form-label">Email</label>
                             <div class="col-sm-9">
-                                <input type="email" name="email" class="form-control"/>
+                                <input type="email" value="{{ $teacher->email }}" name="email" class="form-control"/>
                             </div>
                         </div>
                         <div class="form-group row mb-4">
                             <label class="col-sm-3 col-form-label">Mobile</label>
                             <div class="col-sm-9">
-                                <input type="number" name="mobile" class="form-control"/>
+                                <input type="number" value="{{ $teacher->mobile }}" name="mobile" class="form-control"/>
                             </div>
                         </div>
                         <div class="form-group row mb-4">
                             <label class="col-sm-3 col-form-label">Address</label>
                             <div class="col-sm-9">
-                                <textarea name="address" class="form-control"></textarea>
+                                <textarea name="address" class="form-control">{{ $teacher->address }}</textarea>
                             </div>
                         </div>
                         <div class="form-group row mb-4">
                             <label class="col-sm-3 col-form-label">Image</label>
                             <div class="col-sm-9">
                                 <input type="file" name="image" class="form-control-file" accept="image/*"/>
+                                <img src="{{asset($teacher->image)}}" alt="" width="100" height="100" class="m-2">
                             </div>
                         </div>
+
                         <div class="form-group row justify-content-end">
                             <div class="col-sm-9">
                                 <div>
-                                    <button type="submit" class="btn btn-primary w-md">Create New Teacher</button>
+                                    <button type="submit" class="btn btn-primary w-md">Update Teacher Info</button>
                                 </div>
                             </div>
                         </div>
@@ -53,3 +55,4 @@
     </div>
     </div>
 @endsection
+
--- resources/views/admin/teacher/manage.blade.php
+++ resources/views/admin/teacher/manage.blade.php
@@ -6,28 +6,48 @@
             <div class="card">
                 <div class="card-body">
                     <h4 class="card-title">Manage Teacher</h4>
+                    <p class="text-center text-success">{{ Session::get('message') }}</p>
                     <div class="table-responsive">
                         <table class="table table-bordered table-hover mb-0">
 
                             <thead>
                             <tr>
                                 <th>#</th>
+                                <th width="5%">Image</th>
                                 <th>Full Name</th>
+                                <th>Code</th>
                                 <th>Email</th>
+                                <th>Mobile</th>
+                                <th>Address</th>
+                                <th>Status</th>
                                 <th>Action</th>
                             </tr>
                             </thead>
                             <tbody>
-                            <tr>
-                                <th scope="row">1</th>
-                                <td>Mark</td>
-                                <td>Otto</td>
-                                <td>
-                                    <a href="" class="btn btn-success btn-sm">
-                                        <i class="fa fa-edit"></i>
-                                    </a>
-                                </td>
-                            </tr>
+                            @foreach($teachers as $teacher)
+                                <tr>
+                                    <th scope="row">{{ $loop->iteration }}</th>
+                                    <td><img src="{{ asset($teacher->image) }}" alt="{{ $teacher->name }}" width="100%"></td>
+                                    <td>{{ $teacher->name }}</td>
+                                    <td>{{ $teacher->code }}</td>
+                                    <td>{{ $teacher->email }}</td>
+                                    <td>{{ $teacher->mobile }}</td>
+                                    <td>{{ $teacher->address }}</td>
+                                    @if($teacher->status == 1)
+                                        <td class="badge rounded-pill bg-success text-white">Active</td>
+                                    @else
+                                        <td class="badge rounded-pill bg-danger text-white">Inactive</td>
+                                    @endif
+                                    <td>
+                                        <a href="{{ route('edit-teacher', ['id'=> $teacher->id]) }}" class="btn btn-success btn-sm">
+                                            <i class="fa fa-edit"></i>
+                                        </a>
+                                        <a href="{{ route('delete-teacher', ['id'=> $teacher->id]) }}" class="btn btn-danger btn-sm">
+                                            <i class="fa fa-trash"></i>
+                                        </a>
+                                    </td>
+                                </tr>
+                            @endforeach
                             </tbody>
                         </table>
                     </div>
